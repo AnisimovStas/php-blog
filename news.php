a6c86faf0437f6ddea7e29196aafaa50643d01d2
Anisimov Stanislav <stanislav.anisimov.dev@gmail.com>
2023-03-08 22:41:04 +0300

    adding comments

--- news.php
+++ news.php
@@ -34,14 +34,44 @@
                 $date .= date(' Y', $article->date);
                 ?>
 
-              <p>  <b>Время публикации: </b><?= $date ?></p>
-
+                <p> <b>Время публикации: </b><?= $date ?></p>
             </div>
+            <h3 class="mt-3"> Комментарии:</h3>
+            <form action="/news.php?id=<?= $_GET['id'] ?>" method='post'>
+                <div class="form-floating">
+                    <input type="text" class="form-control" name="username" value="<?= $_COOKIE['loginCookie'] ?>" id="username" placeholder="Введите имя пользователя">
+                    <label for="username">Имя пользователя</label>
+                </div>
+                <div class="form-floating">
+                    <textarea type="text" class="form-control" name="mess" id="mess" placeholder="Введите комментарий"></textarea>
+                    <label for="mess">Комментарии</label>
+                </div>
+                <button type="submit" class="w-100 btn btn-lg btn-warning mb-3" id="sendMess">Добавить комментарий</button>
+            </form>
+            <?php
+            if ($_POST['username'] != '' && $_POST['mess'] != '') {
+                $username = trim(filter_var($_POST['username'], FILTER_SANITIZE_FULL_SPECIAL_CHARS));
+                $mess = trim(filter_var($_POST['mess'], FILTER_SANITIZE_FULL_SPECIAL_CHARS));
 
+                $sql = 'INSERT INTO comments(username, mess, articleId) VALUES (?,?,?)';
+                $query = $pdo->prepare($sql);
+                $query->execute([$username, $mess, $_GET['id']]);
+            }
+            $sql = 'SELECT * FROM comments WHERE articleId = :id ORDER BY id DESC';
+            $query = $pdo->prepare($sql);
+            $query->execute(['id' => $_GET['id']]);
+            $comments = $query->fetchAll(PDO::FETCH_OBJ);
+            foreach ($comments as $comment) {
+                echo "<div class='alert alert-info mb-12'>
+                <h4>$comment->username</h4> 
+                <p>$comment->mess</p>
+                </div>";
+            }
+            ?>
         </div>
         <?php require './blocks/aside.php' ?>
     </main>
-    <div class="absolute"> <?php require './blocks/footer.php' ?> </div>
+    <div> <?php require './blocks/footer.php' ?> </div>
 </body>
 
 </html>
